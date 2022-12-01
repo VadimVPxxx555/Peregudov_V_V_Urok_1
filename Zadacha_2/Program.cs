2307575c1f9db7f34c139b70be2d78ad4923b909
Vadim V P <Everestxxx555@yandex.ru>
2022-12-01 23:25:34 +0300

    коммит 2 Zadacha_2

--- Zadacha_2/Program.cs
+++ Zadacha_2/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите первое число");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = int.Parse(Console.ReadLine());
+if (number1<number2)
+{
+    Console.WriteLine ("первое число меньше второго");
+}
+else
+{
+    Console.WriteLine ("первое число больше второго");
+}
