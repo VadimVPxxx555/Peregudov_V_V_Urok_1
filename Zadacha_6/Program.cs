86e88f72054d6718d842d918dfa5573c36f97734
Vadim V P <Everestxxx555@yandex.ru>
2022-12-02 01:22:09 +0300

    коммит Zadacha_6

---
+++ Zadacha_6/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число");
+int number1 = int.Parse(Console.ReadLine());
+double z =  number1 % 2;
+
+if (z < 1)
+{
+    Console.WriteLine ($"{number1} четное");
+}
+else
+{
+    Console.WriteLine ($"{number1} нечетное");
+}
+
