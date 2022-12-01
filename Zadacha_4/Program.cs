d108230559890d760e764f23a952df8737ff0e7e
Vadim V P <Everestxxx555@yandex.ru>
2022-12-02 00:08:33 +0300

    коммит Zadacha_4

---
+++ Zadacha_4/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int number3 = int.Parse(Console.ReadLine());
+int max = 0;
+if (number1 < number2)
+{
+    max = number2;
+}
+else
+{
+   max = number1;
+}
+
+
+if (number3 < max)
+{
+    
+}
+else
+{
+   max = number3;
+}
+
+Console.WriteLine ($"max = {max}");
