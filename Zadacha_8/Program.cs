4e3deddab4644fdf36c8a672e34c8d640427d46f
Vadim V P <Everestxxx555@yandex.ru>
2022-12-03 15:54:23 +0300

    коммит всего 2

---
+++ Zadacha_8/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+int number1 = 0;
+
+while((number1 + 2) < number)
+{
+    number1 = number1 + 2;
+    Console.Write ($"{number1}, ");
+}
+Console.Write ($"{number}.");
+
